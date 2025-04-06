6d4e625a6c46f3d3c285e49f2bfae8a5c77defd8
(cksdyd3786) <(cksdyd7179@gmail.com)>
2025-04-06 21:26:13 +0900

    modified:   .vscode/settings.json 	deleted:    a.txt 	new file:   result.txt 	new file:   scores.txt 	modified:   test.cpp 	modified:   test.exe

--- test.cpp
+++ test.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    int n, sum = 0;
-    
-    ifstream fin;
-    fin.open("a.txt");
+    double score, sum = 0.0, cnt = 0.0, ave;
+    double maxscore = -100.0;
+    string name, maxname;
 
-    while(fin >> n) {
-        sum += n;
+    ifstream fin;
+    fin.open("scores.txt");
+    if(!fin){
+        cerr<< "Error opening scores.txt"<< endl;
     }
 
+    while(fin >> name >> score){
+        cnt++;
+        sum += score;
+        if(score>maxscore){
+            maxscore = score;
+            maxname = name;
+        }
+    }
+    ave = sum/cnt;
+
     fin.close();
-    cout << "ÇÕ = " << sum << endl; 
+
+    ofstream fout("result.txt");
+    if(!fout){
+        cerr<< "Error opening result.txt"<< endl;
+    }
+
+    fout<< "ÃÑ "<< cnt<< "¸í"<<endl;
+
+    fout<< fixed<< setprecision(2); 
+    fout<< "ÇÕ°è: "<< sum<< endl;
+    fout<< "Æò±Õ: "<< ave<< endl;
+    fout<< "ÃÖ°íÁ¡: "<< maxname<< " "<< maxscore<<endl;
+
+    fout.close();
+
+    cout<< "Results saved in result.txt."<<endl;
     
     return 0;
 }
